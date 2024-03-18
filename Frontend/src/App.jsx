c0abff5c0fc4672274e60336df84731479dc2e38
piyush <piyushjha150403@gmail.com>
2024-03-18 23:37:15 +0530

    deletecarplate

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {  BrowserRouter as Router,Route,Routes , } from 'react-router-dom'
 import Home from './screens/Home'
 import Login from './screens/Login'
 import Signup from './screens/Signup'
-import CarPlateViewer from './screens/CarPlateViewer'
+
 const App = () => {
   return (
    <Router>
